e072b23466ea7007d791d72a0262ec26dfbda6f6
Aaron8957 <omaraaronguerrero@gmail.com>
2025-06-05 12:40:13 -0600

    Correcciones touch

--- Assets/00_PAI/Scripts/Cameras/CameraZoomMapa.cs
+++ Assets/00_PAI/Scripts/Cameras/CameraZoomMapa.cs
@@ -17,7 +17,6 @@ public class CameraZoomMapa : MonoBehaviour
     public float DesplacementZoomIn = 80000;
     public float DesplacementZoomOut = 34000;
     
-    public bool ZoomToPosition = true;
     public double ZoomDeatZone = 0.03f;
     public float zoomDelta;
     public float zoomValMultiply = 0.5f;
@@ -36,12 +35,6 @@ public class CameraZoomMapa : MonoBehaviour
         var newPos = transform.localPosition;
         newPos.y = zoomDownPivot.transform.localPosition.y * zoomVal;
         transform.localPosition = newPos;
-
-        if (ZoomToPosition)
-        {
-            GetScreenPosition();
-            MoveCameraPosition();
-        }
     }
 
     public void ResetZoom()
@@ -49,82 +42,30 @@ public class CameraZoomMapa : MonoBehaviour
         SetZoom(resetZoomVal);
     }
     
-    public void AddToZoomInverted(float val)
-    {
-        if(LeanTouch.Fingers.Count !=2)
-            return;
-        
-        if (InteractionOverUI_List.GetIsInteractionOverUI_List())
-        {
-            return;
-        }
-        
-        if (moveCamera.MapTouchElement != null)
-            if (!moveCamera.MapTouchElement.IsClicOverElement)
-            {
-                return;
-            }
-        
-        //Debug.Log("zoomval: " + (val-1));
-        if (Mathf.Abs(val-1) > ZoomDeatZone)
-            AddToZoom((val-1) * zoomValMultiply);
-    }
+    // public void AddToZoomInverted(float val)
+    // {
+    //     if(LeanTouch.Fingers.Count !=2)
+    //         return;
+    //     
+    //     if (InteractionOverUI_List.GetIsInteractionOverUI_List())
+    //     {
+    //         return;
+    //     }
+    //     
+    //     if (moveCamera.MapTouchElement != null)
+    //         if (!moveCamera.MapTouchElement.IsClicOverElement)
+    //         {
+    //             return;
+    //         }
+    //     
+    //     //Debug.Log("zoomval: " + (val-1));
+    //     if (Mathf.Abs(val-1) > ZoomDeatZone)
+    //         AddToZoom((val-1) * zoomValMultiply);
+    // }
     
     public void AddToZoom(float val)
     {
         zoomIncrement = val;
         SetZoom(Mathf.Clamp01(zoomVal + val));
-    }
-
-    public void GetScreenPosition()
-    {
-        if (LeanTouch.Fingers.Count == 2)
-        {
-            //normalizado -1 a 1
-            if (moveCamera.useElementUI)
-            {
-                screenPos = moveCamera.MapTouchElement.GetPoitInUIElement(
-                    LeanGesture.GetScreenCenter(LeanTouch.Fingers));
-                screenPos.x = (screenPos.x) * 2 - 1;
-                screenPos.y = (screenPos.y) * 2 - 1;
-            }
-            else
-            {
-                screenPos = LeanGesture.GetScreenCenter(LeanTouch.Fingers);
-                screenPos.x = (screenPos.x / Screen.width) * 2 - 1;
-                screenPos.y = (screenPos.y / Screen.height) * 2 - 1;
-            }
-        }
-    }
-
-    public virtual void MoveCameraPosition()
-    {
-        if (moveCamera != null)
-        {
-            if (zoomIncrement > 0)
-            {
-                moveCamera.MoveCameraDisplacemment(
-                    new Vector3(screenPos.x, 0, screenPos.y) * 
-                    (DesplacementZoomIn * (1 - zoomVal) * zoomIncrement));
-            }
-            else
-            {
-                if (zoomVal > 0.1f)
-                {
-                    Vector3 mov = moveCamera.transform.InverseTransformPoint(moveCamera.OrigenPos).normalized;
-
-                    relativePoint.x = mov.x;
-                    relativePoint.z = mov.z;
-                    
-                    moveCamera.MoveCameraDisplacemment(relativePoint * 
-                                                       (DesplacementZoomOut * (1f - zoomVal) * Mathf.Abs(zoomIncrement)));
-                }
-                else
-                {
-                    //moveCamera.MoveCameraDisplacemment(Vector3.zero);
-                    moveCamera.MoveCameraDisplacemment(moveCamera.OrigenPos - moveCamera.transform.position);
-                }
-            }
-        }
     }
 }
--- Assets/00_PAI/Scripts/Input/ControlCameraTouch.cs
+++ Assets/00_PAI/Scripts/Input/ControlCameraTouch.cs
@@ -9,9 +9,14 @@ public class ControlCameraTouch : MonoBehaviour
     public float MultiplyInputTiltTouch;
     public float MultiplyInputMoveTouchMax;
     public float MultiplyInputMoveTouchMin;
+    public float MultiplyInputMoveZoomTouch;
 
     public float distance, distanceAnt;
+    public float zoomIncrement;
     public bool isInitTouch2Fingers;
+    public bool moveCameraZoom;
+
+    public Vector2 posCenterZoom;
         
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,46 +37,77 @@ public class ControlCameraTouch : MonoBehaviour
     
     public void InputTouch()
     {
-            if (cameraMap != null)
+        if (cameraMap != null)
+        {
+            if (LeanTouch.Fingers.Count == 1)
             {
-                if (LeanTouch.Fingers.Count == 1)
+                if (!LeanTouch.GuiInUse && !InteractionOverUI_List.GetIsInteractionOverUI_List())
                 {
-                    if (!LeanTouch.GuiInUse && !InteractionOverUI_List.GetIsInteractionOverUI_List())
-                    {
-                        cameraMap.SetTouchInputDrag(new Vector2(
-                            LeanTouch.Fingers[0].ScaledDelta.x * GetMultipliInputTouch(),
-                            LeanTouch.Fingers[0].ScaledDelta.y * GetMultipliInputTouch()));
-                    }
-                }
-
-                if (LeanTouch.Fingers.Count == 2)
-                {
-                    if (!LeanTouch.GuiInUse && !InteractionOverUI_List.GetIsInteractionOverUI_List())
-                    {
-                        distance = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
-                            LeanTouch.Fingers[1].ScreenPosition);
-
-                        if (isInitTouch2Fingers)
-                        {
-                            cameraMap.SetTouchInputZoom((distance - distanceAnt) * MultiplyInputZoomTouch);
-                        }
-
-                        distanceAnt = distance;
-                        isInitTouch2Fingers = true;
-                    }
-                }
-                else
-                {
-                    isInitTouch2Fingers = false;
-                }
-
-                if (LeanTouch.Fingers.Count == 3)
-                {
-                    if (!LeanTouch.GuiInUse && !InteractionOverUI_List.GetIsInteractionOverUI_List())
-                    {
-                        cameraMap.SetTouchInputTilt(LeanTouch.Fingers[0].ScaledDelta.y * MultiplyInputTiltTouch);
-                    }
+                    cameraMap.SetTouchInputDrag(new Vector2(
+                        LeanTouch.Fingers[0].ScaledDelta.x * GetMultipliInputTouch(),
+                        LeanTouch.Fingers[0].ScaledDelta.y * GetMultipliInputTouch()));
                 }
             }
+
+            if (LeanTouch.Fingers.Count == 2)
+            {
+                if (!LeanTouch.GuiInUse && !InteractionOverUI_List.GetIsInteractionOverUI_List())
+                {
+                    distance = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
+                        LeanTouch.Fingers[1].ScreenPosition);
+
+                    if (isInitTouch2Fingers)
+                    {
+                        zoomIncrement = (distance - distanceAnt) * MultiplyInputZoomTouch;
+                        cameraMap.SetTouchInputZoom(zoomIncrement);
+                        
+                        if (moveCameraZoom)
+                        {
+                            posCenterZoom = (LeanTouch.Fingers[0].ScreenPosition + 
+                                        (LeanTouch.Fingers[1].ScreenPosition - LeanTouch.Fingers[0].ScreenPosition)) /
+                                        new Vector2(Screen.width, Screen.height) * new Vector2(2, 2) - new Vector2(1,1);
+                        
+                            MoveCameraPosition(posCenterZoom, zoomIncrement);
+                        }
+                    }
+
+                    distanceAnt = distance;
+                    isInitTouch2Fingers = true;
+                }
+            }
+            else
+            {
+                isInitTouch2Fingers = false;
+            }
+
+            if (LeanTouch.Fingers.Count == 3)
+            {
+                if (!LeanTouch.GuiInUse && !InteractionOverUI_List.GetIsInteractionOverUI_List())
+                {
+                    cameraMap.SetTouchInputTilt(LeanTouch.Fingers[0].ScaledDelta.y * MultiplyInputTiltTouch);
+                }
+            }
+        }
+    }
+    
+    public virtual void MoveCameraPosition(Vector2 desplacementZoomDir, float _zoomIncrement)
+    {
+        if (zoomIncrement > 0)
+        {
+            cameraMap.MoveCameraDisplacemment(new Vector3(desplacementZoomDir.x, 0, desplacementZoomDir.y) * 
+                                              (_zoomIncrement * MultiplyInputMoveZoomTouch));
+        }
+        else
+        {
+            if (cameraMap.zoomValue > 0.00001f)
+            {
+                cameraMap.MoveCameraDisplacemment((cameraMap.OrigenPos - cameraMap.transform.position).normalized  * 
+                                                  (Mathf.Abs(_zoomIncrement * MultiplyInputMoveZoomTouch)));
+            }
+            else
+            {
+                cameraMap.MoveHome();
+            }
+        }
     }
 }
