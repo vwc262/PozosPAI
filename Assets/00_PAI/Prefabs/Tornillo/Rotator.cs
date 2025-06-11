4a0733bca87d7002a6f2437627e44a22131213da
MIKE-RED <miguelrojasmarc99@gmail.com>
2025-06-11 17:55:48 -0600

    Cambios Particular

--- Assets/00_PAI/Prefabs/Tornillo/Rotator.cs
+++ Assets/00_PAI/Prefabs/Tornillo/Rotator.cs
@@ -2,20 +2,17 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public float rotSpeed = 3.5f;
-    public float fill = 0f;
-    public float fillSpeed = 0.6f;
-    public Renderer rend;
     public Vector3 rotDirection;
     void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        //fill += Time.deltaTime * fillSpeed;
+  
         transform.Rotate(rotDirection * rotSpeed * Time.deltaTime);
-       // rend.material.SetFloat("_Threshold", fill);
+
     }
     
 }
