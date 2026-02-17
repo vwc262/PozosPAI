a83a1a9de08b13a4be664f5e3a39347548e6db99
Miguel Angel Rojas <miguelrojasmarc99@gmail.com>
2026-02-17 15:50:36 -0600

    Cambios PAI

--- Assets/00_PAI/Scripts/PantallaResumen/ControlPantallaResumen.cs
+++ Assets/00_PAI/Scripts/PantallaResumen/ControlPantallaResumen.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class ControlPantallaResumen : MonoBehaviour
 {
+    public GameObject panelBarrientos;
+    public GameObject panelNoBarrientos;
     public bool GetShowResumen()
     {
         if (ControlAccesoPozosPAI._singletonExists)
             return ControlAccesoPozosPAI.singleton.configuration.showResumenInit;
 
         return true;
+    }
+
+    public void GetShowCaudales()
+    {
+        if (panelBarrientos != null)
+            panelBarrientos.SetActive(ControlAccesoPozosPAI.singleton.configuration.habilitarBarrientos);
+        
+        if (panelNoBarrientos != null)
+            panelNoBarrientos.SetActive(!(ControlAccesoPozosPAI.singleton.configuration.habilitarBarrientos));
+        
     }
 }
